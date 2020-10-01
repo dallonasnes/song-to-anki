b84ea0a67500cf10d5ac58bdd551ce196f13693f
Dallon Asnes <dallon.asnes@gmail.com>
2020-09-30 23:05:12 -0500

    more todos

--- chrome_extension/server/app.py
+++ chrome_extension/server/app.py
@@ -15,6 +15,8 @@ if __name__ == "__main__":
 #1. submit PR to genanki project so that I can use that repo directly instead of my custom classes
 #2. sync server-only proj back up with improvements to this api
 #3. delete log files periodically on pythonanywhere to save memory?
+#4. write options telling that pages that have the yellow tracking work best
+#5. to minify and combine js content just for fun?
 
 #Need to validate incoming lyrics json obj to make sure nothing malicious
 #Can do this with JWT - have client sent token to verify that my client-side code constructed the object
